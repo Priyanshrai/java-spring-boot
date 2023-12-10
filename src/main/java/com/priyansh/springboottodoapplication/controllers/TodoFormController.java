1141336f60497f30958dbc406787921b7e691e1c
priyansh rai <rai.priyansh007@gmail.com>
2023-12-10 13:43:24 +0530

    Update and Delete Forms

---
+++ src/main/java/com/priyansh/springboottodoapplication/controllers/TodoFormController.java
@@ -0,0 +1,41 @@
+package com.priyansh.springboottodoapplication.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.priyansh.springboottodoapplication.models.TodoItem;
+import com.priyansh.springboottodoapplication.repositories.TodoItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@Controller
+public class TodoFormController {
+    private final Logger logger = LoggerFactory.getLogger(TodoFormController.class);
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateFrom(@PathVariable("id") long id, Model model) {
+        TodoItem todoItem= todoItemRepository.findById(id)
+        .orElseThrow(()-> new IllegalArgumentException("TodoItem id: "+ id + "Not Found"));
+
+        model.addAttribute("todo", todoItem);
+        return "update-todo-item";
+    }
+
+    @GetMapping("/delete/{id}")
+        public String deleteTodoItem (@PathVariable("id") long id, Model model){
+        TodoItem todoItem= todoItemRepository.findById(id)
+        .orElseThrow(()-> new IllegalArgumentException("TodoItem id: "+ id + "Not Found"));
+
+        todoItemRepository.delete(todoItem);
+        return "redirect:/";
+
+        }
+}
+    
